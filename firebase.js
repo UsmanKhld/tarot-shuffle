4c89631bf58d16a9198583ab49f03d6145bce8c7
Usman Khalid <usmankhld602@gmail.com>
2024-11-25 16:24:03 -0600

    small changes

--- firebase.js
+++ firebase.js
@@ -1,21 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import dotenv from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseApiKey = process.env.FIRE_BASE_API_KEY;
+const firebaseAppId = process.env.FIRE_BASE_APP_ID;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyDGTL80Hmi9vxIOgj3AKKJ3pPftMGU_hJQ",
+	apiKey: firebaseApiKey,
 	authDomain: "tarot-shuffle.firebaseapp.com",
 	projectId: "tarot-shuffle",
 	storageBucket: "tarot-shuffle.firebasestorage.app",
 	messagingSenderId: "317465772805",
-	appId: "1:317465772805:web:cedcd33843f1d3a9e3ad6c",
+	appId: firebaseAppId,
 	measurementId: "G-5TMGM4Z97D",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+connectAuthEmulator(auth, "http://localhost:9099");
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,7 @@
 import Home from "./Home/Home";
 import Decks from "./Decks/Decks";
 import Settings from "./Settings/Settings";
+import Login from "./Login/Login";
+import SignUp from "./SignUp/SignUp";
 
-export { Home, Decks, Settings };
+export { Home, Decks, Settings, Login, SignUp };
